dcc51f411b8ba3f81718985ce22bc4c25266433a
Khadija <khadijabeja@gmail.com>
2023-11-30 20:15:16 +0300

    updates

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,7 @@
 word="Holberton"
-print(word[0:3])
-print(word[7:9])
-print(word[1:8])
-print(len("olberto"))
+#print(word "first three letters"[0:3])
+#print(word[7:9])
+#print(word[1:8])
+#print(len("olberto"))
+#print("First 3 letters: {}".format[0:3])
+print("word: {}".format("word_first_three"))
