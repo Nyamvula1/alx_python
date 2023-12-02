1f98dd50060bf28df93fcfd86fcad67a0965a856
Khadija <khadijabeja@gmail.com>
2023-12-02 17:00:54 +0300

    updates

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,7 +1,8 @@
 def fibonacci_sequence(n):
     n=int(input())
-    if n <= 1:
-        return n
+    if n == 0:
+      return 0
+    elif n == 1:
+      return 1
     else:
-        return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
-
+      return fibonacci_sequence(n-1)+fibonacci_sequence(n-2)
