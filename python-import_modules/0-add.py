724b0f51f84c8f8bf2c1f43057da250bf5e6631b
Khadija <khadijabeja@gmail.com>
2023-12-05 19:21:28 +0300

    import add module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
     a=1
     b=2
-        
+    c=a+b     
 import add_0 
-c=a+b 
+add=a+b
 print("{}+{}={}" .format(a, b, c))
 
---
+++ python-import_modules/mymodule.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':   
+    a = 1
+    b = 2
+    c = a + b
+
+    from add_0 import add
+    print(add("{} + {} = {}".format(a, b, c)))
