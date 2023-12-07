34fecdc4c1adba5faa15745d7214b8ef8ec8750b
Khadija <khadijabeja@gmail.com>
2023-12-07 21:44:02 +0300

    updates

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -15,9 +15,5 @@ if __name__ == '__main__':
     result = safe_print_division(a, b)
     print("{:d} / {:d} = {}".format(a, b, result))
 
-    a = 12
-    b = 0
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result)) 
     
         
