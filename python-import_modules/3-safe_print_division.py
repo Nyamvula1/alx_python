d38bd0d00231b5588b38edb96f0c927975583cd1
Khadija <khadijabeja@gmail.com>
2023-12-07 21:40:58 +0300

    error handling

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,13 +1,23 @@
 def safe_print_division(a, b):
-    if __name__ == '__main__':
-        try:
-            result = a / b
-        except ZeroDivisionError:
-            result = None
-        finally:
-            return "inside result {}".format(result)
-        
-        
     
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print ("inside result {}".format(result))
+        return result
+
+        
+if __name__ == '__main__':        
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result)) 
     
         
