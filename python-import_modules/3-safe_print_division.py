551306426f86ac3561296f69dab90c618be0d299
Khadija <khadijabeja@gmail.com>
2023-12-07 16:38:57 +0300

    zero division error

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,11 @@
 def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-         return "inside result {}".format(result)
+    if __name__ == '__main__':
+        try:
+            result = a / b
+        except ZeroDivisionError:
+            result = None
+        finally:
+            return "inside result {}".format(result)
         
         
     
