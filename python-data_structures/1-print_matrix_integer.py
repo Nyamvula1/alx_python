6d4eaf41c733341c65f334471ec9005bc560e2c5
Khadija <khadijabeja@gmail.com>
2023-12-10 18:54:51 +0300

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,19 @@
 def square_matrix(matrix=[]):
     result=[]
-    for i in matrix:
-        square_list = []
-        for element in i:
-            square_list.append(element ** 2)
-        result.append(square_list)
+    for row in matrix:
+        square_row = []
+        for element in row:
+            square_row.append(element ** 2)
+        result.append(square_row)
     return result
+
+result = square_matrix(matrix=[])
+for row in result:
+    formatted_row = ', '.join(map("{}".format, row))
+    print("[{}],".format(formatted_row))
+
+
+
 
 
 
