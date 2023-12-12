3a17ea5d6998539646b68303818a07cbc1ba33e0
Khadija <khadijabeja@gmail.com>
2023-12-12 16:05:16 +0300

    common elements

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,10 +1,25 @@
 def common_elements(set_1, set_2):
-    set_1 = { "Python", "C", "Javascript" }
-    set_2 = { "Bash", "C", "Ruby", "Perl" }
+    return set_1.intersection(set_2)
     
-    return set_1, set_2
 if __name__ == '__main__':
-    set_1, set_2  = common_elements(set(), set())
-    c_set = set_1.intersection(set_2)
-    print(sorted(list(c_set)))
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    
+    c_set = common_elements(set_1, set_2)
+    
+    if c_set:
+        for element in sorted(c_set):
+            print(element)
+    else:
+        print("No common elements.")
+        
+        
+        
+# for i in set_1, set_2:
+     #   return i
+#if __name__ == '__main__':
+   # set_1 = { "Python", "C", "Javascript" }
+    #set_2 = { "Bash", "C", "Ruby", "Perl" }
+    #c_set = common_elements(set_1, set_2)
+    #print(sorted(list(c_set)))
     
