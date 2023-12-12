718607c69cd77be03a20415daafc07e0e1b8b876
Khadija <khadijabeja@gmail.com>
2023-12-12 14:13:34 +0300

    MULTIPLE RETURNS

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,11 +1,12 @@
 def multiple_returns(sentence):
-    if sentence == (""):
-        return None
-    else:
-        length = len("")
-        first_char = sentence[0]
-        return length, first_char
     
+    if sentence == "":
+        return None
+
+    length = len(sentence)
+    first_char = sentence[0]
+    return length, first_char
+
 if __name__ == '__main__':
     sentence = "At Holberton school, I learnt C!"
     length, first = multiple_returns(sentence)
