649242e7b33e1f6c6a387397d167ffddb56460a5
Khadija <khadijabeja@gmail.com>
2023-12-14 21:48:21 +0300

    square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,12 @@
 class Square():
     def __init__(self, size=0):
         self.__size = size
-        
+        self.dict__ = {size : self.__size}
+ #This indicates that mysquare is an instance of the Square class, and the dictionary containing the size is printed as expected.       
 if __name__ == "__main__":
     my_square = Square(3)
-    print(my_square.__size)
-    print(my_square.__dict__)
+    print(type(my_square.__size))
+    print(my_square.dict__)
 
 try:
     print(my_square.__size)
