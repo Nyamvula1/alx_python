cfed4d51a713ea7686f8b92d39a580e20e0ce27b
Khadija <khadijabeja@gmail.com>
2023-12-14 21:37:03 +0300

    Square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,27 +3,16 @@ class Square():
         self.__size = size
         
 if __name__ == "__main__":
-    my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
-
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+    square_1 = Square(3)
+    print(square_1.__size)
+    print(square_1.__dict__)
 
 try:
-    print(my_square_1.size)
+    print(square_1.__size)
 except Exception as e:
     print(e)
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
+    
+
+
