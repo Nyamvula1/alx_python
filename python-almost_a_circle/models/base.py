a9ccd4824ec47f1f617c5df9cb40bd5c6311b9d8
Khadija <khadijabeja@gmail.com>
2023-12-17 00:23:59 +0300

    base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 '''my base class'''
-class base:
+class Base:
    
     '''we define a class named Base with a private class attribute __nb_objects'''
     __nb_objects = 0
@@ -10,6 +10,6 @@ class base:
             self.id = id
         else:
             '''the base class is the increament setting'''
-            base. __nb_objects += 1
-            self.id = base.__nb_objects
+            Base. __nb_objects += 1
+            self.id = Base.__nb_objects
 
