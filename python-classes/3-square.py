ba43e2409f7a4e13c80a307f105fc5b899e9b2bd
Khadija <khadijabeja@gmail.com>
2023-12-16 21:54:16 +0300

    3 square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,9 +5,11 @@ class Square:
     '''initializing size of the square'''
     def __init__(self, size=0):
         self.size = size
+    '''initializing the getter'''
     @property
     def size(self):
         return self.__size 
+    '''initializing the setter'''
     @size.setter
     def size(self, value):
         if not isinstance(value,int):
@@ -16,6 +18,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+    
     def area(self):
         return self.__size ** 2
 if __name__ == '__main__':
