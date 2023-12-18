db42bcce5d92774df8c6fef3c85ea169aa5448ab
Khadija <khadijabeja@gmail.com>
2023-12-18 20:50:00 +0300

    4-base geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,6 @@
+'''defining the class'''
+class BaseGeometry:
+    '''initializing the method:area'''
+    def area(self):
+        '''raises the message'''
+        raise Exception('area() is not implemented')
