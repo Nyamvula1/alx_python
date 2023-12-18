d8f692cbd8dbc541985cff3ecd51feb9917d68bb
Khadija <khadijabeja@gmail.com>
2023-12-18 20:44:32 +0300

    base geometry

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,7 @@
+'''this is an empty class'''
+class BaseGeometry:
+    '''we use the pass keyword 
+    when we don't want to add any other properties 
+    or methods to the class
+    '''
+    pass
