39abf57d6370da5ab3ff7b6d8dd1e2aca57d6f97
Khadija <khadijabeja@gmail.com>
2023-12-18 20:24:22 +0300

    is kind of class

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,8 @@
+'''
+This function is_same_class takes two arguments, obj and a_class, 
+and it returns True if the class of obj is the same as the specified a_class, 
+and False otherwise.
+'''
+def is_kind_of_class(obj, a_class):
+    '''to check if it is true or false'''
+    return isinstance(obj, a_class) 
