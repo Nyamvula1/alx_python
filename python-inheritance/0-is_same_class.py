1bf0ae94682601744d1c1c4d26d7aed2cf0a6c92
Khadija <khadijabeja@gmail.com>
2023-12-18 19:28:14 +0300

    is same class

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -10,27 +10,33 @@ You initialize a variable a with the value 1.
 '''
 a = 1
 '''
-Here, you use the is_same_class function to check whether 
-a is an instance of the classes int, float, and object.
-If a is an instance of a class, it prints a corresponding message.
-'''
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-elif is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-elif is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
-'''
 You call the is_same_class function for each of the classes (int, float, and object) 
 and store the results in result_int, result_float, and result_object. 
 Then, you print these results.
 '''
-#result_int = is_same_class(a, int)
-#result_float = is_same_class(a, float)
-#result_object = is_same_class(a, object)
+result_int = is_same_class(a, int)
+result_float = is_same_class(a, float)
+result_object = is_same_class(a, object)
 
-#print(result_int)     # Output: True
-#print(result_float)   # Output: False
-#print(result_object)
+print(result_int)     # Output: True
+print(result_float)   # Output: False
+print(result_object)
+
+
+'''
+Here, you use the is_same_class function to check whether 
+a is an instance of the classes int, float, and object.
+If a is an instance of a class, it prints a corresponding message.
+'''
+#if is_same_class(a, int):
+    #print("{} is an instance of the class {}".format(a, int.__name__))
+#elif is_same_class(a, float):
+    #print("{} is an instance of the class {}".format(a, float.__name__))
+#elif is_same_class(a, object):
+    #print("{} is an instance of the class {}".format(a, object.__name__))
+
+
+
+
 
 
