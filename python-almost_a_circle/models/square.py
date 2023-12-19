edaa4f83895044e853433259f1c88fb4085633d5
Khadija <khadijabeja@gmail.com>
2023-12-19 23:07:15 +0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 '''imports from rectangle class'''
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 class Square(Rectangle):
     '''initializing the class'''
     def __init__(self, size, x=0, y=0, id=None):
@@ -9,3 +9,32 @@ class Square(Rectangle):
     '''method for formatting the string'''
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
+    '''function for getting and setting the attributes and returns the width and height'''
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+    
+    @property
+    def size(self):
+        return self.height
+    
+    @size.setter
+    def size(self, value):
+        self.height = value
+
+# if __name__ == "__main__":
+
+#     s1 = Square(5)
+#     print(s1)
+#     print(s1.size)
+#     s1.size = 10
+#     print(s1)
+
+#     try:
+#         s1.size = "9"
+#     except Exception as e:
+#         print("[{}] {}".format(e.__class__.__name__, e))
