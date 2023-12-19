d431e982cc669b12c1c54f797dd27b3427f12679
Khadija <khadijabeja@gmail.com>
2023-12-19 22:54:34 +0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,11 @@
+'''imports from rectangle class'''
 from rectangle import Rectangle
 class Square(Rectangle):
+    '''initializing the class'''
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
+        '''calling the class using super'''
         super().__init__(id, x, y, size, size)
-    
+    '''method for formatting the string'''
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
