dcde5fb65bb3c44830177a2ef693c0ebf4c8e4fa
Khadija <khadijabeja@gmail.com>
2023-12-19 22:57:35 +0300

    updates

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -115,4 +115,4 @@ class Rectangle(Base):
     
           
 
-           
+  
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,7 +5,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         '''calling the class using super'''
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
     '''method for formatting the string'''
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
