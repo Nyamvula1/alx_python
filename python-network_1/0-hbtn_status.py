119e1d8d6268195724d7c298e524861028a49c7e
Khadija <khadijabeja@gmail.com>
2023-12-21 00:11:42 +0300

    requests

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,5 +3,5 @@ import requests
 url = 'https://alu-intranet.hbtn.io/status'
 response= requests.get(url)
 print('Body response:')
-print('\t - type: {}'.format(type(response.text)))
-print('\t - content: {}'.format((response.text)))
+print('\t- type: {}'.format(type(response.text)))
+print('\t- content: {}'.format((response.text)))
