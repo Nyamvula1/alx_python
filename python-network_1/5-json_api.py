d757c48061d40e2d3f19b2a5319d6faef244f5e3
Khadija <khadijabeja@gmail.com>
2023-12-21 20:11:04 +0300

    json

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,9 +1,9 @@
 import sys
 import requests
 
-url= input('Enter url: ') if len(sys.argv) < 2 else sys.argv[1]
+url= 'http://0.0.0.0:5000/search_user'
 '''this condition checks for the lenght of the arguments in the command line , if the length is 1 then it returns an empty string'''
-q =''if len(sys.argv) == 1 else sys.argv[2]
+q =''if len(sys.argv) == 1 else sys.argv[1]
 '''payload-the letter is sent to the server in the format of a dictionary in the payload'''
 payload = {'our_letter': q}
 response= requests.post(url=url, data=payload)
