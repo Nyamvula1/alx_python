26815641bf3305a0b244029c0d85ce75c4c0d64a
Khadija <khadijabeja@gmail.com>
2023-12-21 19:09:39 +0300

    error code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,7 @@
+import sys
+import requests
+
+url = input('Enter url= ') if len(sys.argv) < 2 else sys.argv[1]
+response=requests.get(url)
+if response.status_code <= 400:
+    print('error code:{}'.format(response.status_code))
