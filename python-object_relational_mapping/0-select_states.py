59461c978b65d2868905ded0b8a7aa4560ef768b
Khadija <khadijabeja@gmail.com>
2024-01-15 22:31:57 +0300

    STATES

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+# Lists all states from the database hbtn_0e_0_usa.
+import MySQLdb
+from sys import argv
+
+mysql_username = argv[1]
+mysql_pass = argv[2]
+db_name = argv[3]
+
+my_conn = MySQLdb.connect(host = 'localhost',mysql_pass = 'passwd',port=3306,mysql_username = 'user',db_name='db')
+c = my_conn.cursor()
+my_conn.execute("SELECT * FROM `states`")
+[print(state) for state in my_conn.fetchall()]
