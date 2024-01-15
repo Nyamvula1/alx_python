be6d2b787f5e4541122bc5b66f8f0284cd8eeeb3
Khadija <khadijabeja@gmail.com>
2024-01-15 23:19:43 +0300

    states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,13 @@ if __name__ == "__main__":
     mysql_pass = argv[2]
     db_name = argv[3]
 
+    #this is an object method
     my_conn = MySQLdb.connect(host = 'localhost', passwd = mysql_pass, port=3306, user = mysql_username, db = db_name)
+    #cursor is a function in the mysqldb connect used to execute functions
     c = my_conn.cursor()
     c.execute("SELECT * FROM `states`")
-    [print(state) for state in my_conn.fetchall()]
+    rows= c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    my_conn.close()
