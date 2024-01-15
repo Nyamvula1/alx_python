2831f09479232be09bcefd0de9a873625b8029e1
Khadija <khadijabeja@gmail.com>
2024-01-15 22:58:30 +0300

    states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,8 @@ mysql_username = argv[1]
 mysql_pass = argv[2]
 db_name = argv[3]
 
-my_conn = MySQLdb.connect(host = 'localhost',mysql_pass = 'passwd',port=3306,mysql_username = 'user',db_name='db')
-c = my_conn.cursor()
-my_conn.execute("SELECT * FROM `states`")
-[print(state) for state in my_conn.fetchall()]
+if __name__ == "__main__":
+    my_conn = MySQLdb.connect(host = 'localhost',mysql_pass = 'passwd',port=3306,mysql_username = 'user',db_name='db')
+    c = my_conn.cursor()
+    my_conn.execute("SELECT * FROM `states`")
+    [print(state) for state in my_conn.fetchall()]
