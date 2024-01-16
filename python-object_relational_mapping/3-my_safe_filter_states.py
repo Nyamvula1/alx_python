190a9f00e3095b5d059937f4a90ee658ac0c8d89
Khadija <khadijabeja@gmail.com>
2024-01-16 17:35:25 +0300

    MY_SAFE_FILTER_STATES

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,6 +9,13 @@ if __name__ == "__main__":
     # Use query to select records where the second column matches the provided state name
     query = "SELECT * FROM `states` WHERE name = %s"
     c.execute(query, (sys.argv[4],))
+    #Define a parameterized SQL query to select all 
+    # records from the states table where the name 
+    # column matches the provided state name. 
+    # The %s is a placeholder for the parameter.
+    # The execute method is then used to execute the query,
+    # and the second argument is a tuple 
+    # containing the value to be substituted for the %s placeholder.
 
     # Fetch and print the results
     [print(state) for state in c.fetchall()]
