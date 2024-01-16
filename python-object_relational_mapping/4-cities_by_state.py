3534b1824db8e2820ec61ed2c5e342d307a73f73
Khadija <khadijabeja@gmail.com>
2024-01-16 17:51:05 +0300

    CITIES

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,6 +4,10 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
+    
+    #Execute an SQL query to select specific columns 
+    # (id, name from cities and name from states) using an inner join on the state_id column.
+    # The result set is ordered by the id column of the cities table.
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                  FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
