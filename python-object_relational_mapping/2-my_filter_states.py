26568625dafc29d9a1d730388f95f3201ca14495
Khadija <khadijabeja@gmail.com>
2024-01-16 17:16:31 +0300

    FILTER STATES

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,41 @@
 import sys
 import MySQLdb
 
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
+
+
+
 # Check if the required command line arguments are provided
-if len(sys.argv) != 5:
-    print("Usage: python script.py <username> <password> <database> <state_name>")
-    sys.exit(1)
+#if len(sys.argv) != 5:
+    #print("Usage: python script.py <username> <password> <database> <state_name>")
+    #sys.exit(1)
 
 # Get user input from command line arguments
-username, password, database, state_name = sys.argv[1:5]
+#username, password, database, state_name = sys.argv[1:5]
 
 # Connect to the MySQL database
-db = MySQLdb.connect(user=username, passwd=password, db=database)
-c = db.cursor()
+#db = MySQLdb.connect(user=username, passwd=password, db=database)
+#c = db.cursor()
 
 # Use format to create the SQL query with user input
-query = "SELECT * FROM `states` WHERE LOWER (`name`) = LOWER('{}') ORDER BY `id`".format(state_name)
-c.execute(query)
+#query = "SELECT * FROM `states` WHERE LOWER (`name`) = LOWER('{}') ORDER BY `id`".format(state_name)
+#c.execute(query)
 
 # Fetch and print the results
-for state in c.fetchall():
-    print(state)
+#for state in c.fetchall():
+    #print(state)
 
 # Close the database connection
-db.close()
+#db.close()
 
 
 
