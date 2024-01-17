67df141a56fea30c0c95f4f309e133688e142882
Khadija <khadijabeja@gmail.com>
2024-01-18 00:22:05 +0300

    model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,10 @@
+'''module to be imported'''
 from sqlalchemy from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class state(Base):
-    #The State class inherits from Base and is linked to the MySQL table 'states'. It has two class attributes, id and name, representing columns in the database.
+    '''The State class inherits from Base and is linked to the MySQL table 'states'. It has two class attributes, id and name, representing columns in the database.'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name =  Column(String(128), nullable= False)
