9af61c7b63e3241e1c169a544bcf934dd2aa4b87
Khadija <khadijabeja@gmail.com>
2024-01-18 00:28:16 +0300

    model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,30 @@
-'''module to be imported'''
-from sqlalchemy from sqlalchemy import Column, Integer, String
+'''Module to be imported'''
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-'''state class'''
-class state(Base):
-    '''The State class'''
-    
+
+class State(Base):
+    '''State class'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable= False)
+    name =  Column(String(128), nullable=False)
+
+
+
+
+
+# '''module to be imported'''
+# from sqlalchemy from sqlalchemy import Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+
+# Base = declarative_base()
+# '''state class'''
+# class state(Base):
+#     '''The State class'''
+    
+#     __tablename__ = 'states'
+#     id = Column(Integer, primary_key=True)
+#     name =  Column(String(128), nullable= False)
