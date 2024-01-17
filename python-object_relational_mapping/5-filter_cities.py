2d97ff5ac0e7eed5cc9d1dea7aeefa92a9195a4b
Khadija <khadijabeja@gmail.com>
2024-01-17 23:36:04 +0300

    5FILTER

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,10 +13,21 @@ if __name__ == "__main__":
                WHERE states.name = %s \
                ORDER BY cities.id""")
                
-    c.execute(query, (sys.argv[4]))
-    # Print the results as a comma-separated string
+    c.execute(query, (sys.argv[4],))
     cities = c.fetchall()
-    #join is used in joining the values in the list
     print(", ".join(city[0] for city in cities))
-    
+
+    # Close the cursor and database connection
     c.close()
+    db.close()
+    # Print the results as a comma-separated string
+    # states = c.fetchall()
+    # #join is used in joining the values in the list
+    # for i, state in enumerate (states):
+    #     if i == len(states)-1:
+    #         print(", ".join(states[0], end = '' ))
+    #     else:
+    #         print("".join(state[0], end=','))
+    
+    
+    # c.close()
