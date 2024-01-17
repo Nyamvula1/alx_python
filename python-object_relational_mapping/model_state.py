de470e5dac3e0fb78430ae388e6e2fec06796201
Khadija <khadijabeja@gmail.com>
2024-01-18 00:12:50 +0300

    model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,8 +3,8 @@ from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 
-#The State class inherits from Base and is linked to the MySQL table 'states'. It has two class attributes, id and name, representing columns in the database.
 class state(Base):
+    #The State class inherits from Base and is linked to the MySQL table 'states'. It has two class attributes, id and name, representing columns in the database.
     __tablename__ = 'states'
     id = column(int(11) NOT NULL AUTO_INCREMENT)
     name = name = column(varchar(128) NOT NULL)
