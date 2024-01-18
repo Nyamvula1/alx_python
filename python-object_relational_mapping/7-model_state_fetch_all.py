2418fe6e99fa322a2acc93150f184ed17137b5ad
Khadija <khadijabeja@gmail.com>
2024-01-18 21:18:29 +0300

    MODEL STATE

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,51 @@
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+def print_states(username, password, database):
+    # Connect to the database
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost/{database}", pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    # Retrieve and print states
+    states = session.execute(select(State).order_by(State.id)).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+if __name__ == "__main__":
+    # Check if enough command-line arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    # Get user input from command line arguments
+    username, password, database = sys.argv[1:4]
+
+    # Call the function to print state information
+    print_states(username, password, database)
+
+
+
+
+
+
+
+
+
+
+# import sys
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from model_state import State
+
+# if __name__ == "__main__":
+#     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+#                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+#                            pool_pre_ping=True)
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     for state in session.query(State).order_by(State.id):
+#         print("{}: {}".format(state.id, state.name))
