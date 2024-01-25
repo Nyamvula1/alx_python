ab2a61465825a9df3a1921c4a8006614351edaae
Khadija <khadijabeja@gmail.com>
2024-01-25 17:35:39 +0300

    0-HELLO

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,12 +1,11 @@
-'''
-The first flask project
-'''
+'''import flask server'''
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello_world():
+        
+@app.route("/",strict_slashes=False)
+def Hello_HBNB():
     return "Hello HBNB!"
 
-app.run()
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000)
