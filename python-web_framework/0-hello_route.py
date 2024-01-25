0a524dc72dac24722a51fe6f86c990bd81cac2e8
Khadija <khadijabeja@gmail.com>
2024-01-25 17:27:03 +0300

    Hello world

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,12 @@
+'''
+The first flask project
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+app.run()
