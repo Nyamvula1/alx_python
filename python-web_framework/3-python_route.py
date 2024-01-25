f20b29da0d9a63054c1f6df49772ef40e3c87360
Khadija <khadijabeja@gmail.com>
2024-01-25 18:08:41 +0300

    Python_cool

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,30 @@
+'''import flask server'''
+from flask import Flask
+app = Flask(__name__)
+
+        
+@app.route("/",strict_slashes=False)
+
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def HBNB():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def khadija(text):
+    formated_text = text.replace("_", " ")
+    return "C {}".format(formated_text)
+
+    #return f"C {formated_text}" 
+
+@app.route("/python/<text>")
+@app.route("/python/")
+def python_text(text = "is cool"):
+    formated_text = text.replace("_", " ")
+    return "Python {}".format(formated_text)
+    
+    
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000)
