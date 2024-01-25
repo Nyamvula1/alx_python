ded3c1d38196cd53425dec7a90daa5b88f50e39b
Khadija <khadijabeja@gmail.com>
2024-01-25 17:59:49 +0300

    2-C DISPLAY

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,23 @@
+'''import flask server'''
+from flask import Flask
+app = Flask(__name__)
+
+        
+@app.route("/",strict_slashes=False)
+
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def HBNB():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def khadija(text):
+    formated_text = text.replace("_", " ")
+    return "C {}".format(formated_text)
+
+    #return f"C {formated_text}" 
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000)
